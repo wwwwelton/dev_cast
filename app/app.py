b9beaf4011459fb25e8732c78abcc583a077b682
Welton Leite <av4t@hotmail.com>
2024-12-31 06:12:58 -0300

    feat: add create stream route

--- app/app.py
+++ app/app.py
@@ -2,8 +2,9 @@ import os
 import threading
 
 import ffmpeg
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
+from app import db
 from models.models import Destination, Stream
 
 app_bp = Blueprint("app", __name__)
@@ -41,6 +42,53 @@ def get_db():
         ),
         200,
     )
+
+
+@app_bp.route("/streams", methods=["POST"])
+def create_stream_route():
+    try:
+        data = request.get_json()
+
+        if not data:
+            return (
+                jsonify({"message": "Invalid JSON body provided"}),
+                400,
+            )
+
+        stream_name = data.get("stream_name")
+
+        if not stream_name:
+            return (
+                jsonify({"message": "The 'stream_name' field is required"}),
+                400,
+            )
+
+        if Stream.query.filter_by(stream_name=stream_name).first():
+            return (
+                jsonify({"message": "The 'stream_name' already exists"}),
+                400,
+            )
+
+        new_stream = Stream(stream_name=stream_name)
+
+        db.session.add(new_stream)
+        db.session.commit()
+
+        return (
+            jsonify(
+                {
+                    "message": "Stream added successfully!",
+                    "stream": {
+                        "stream_name": new_stream.stream_name,
+                        "streaming_key": new_stream.unique_url,
+                    },
+                }
+            ),
+            201,
+        )
+
+    except Exception:
+        return (jsonify({"message": "An internal server error occurred"}), 500)
 
 
 @app_bp.route("/", methods=["GET"])
