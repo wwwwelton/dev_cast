0ec99931743db2672dd582ad26347fe4c231dbb3
Welton Leite <av4t@hotmail.com>
2024-12-31 14:26:55 -0300

    feat: add update destination route

--- app/destination_routes.py
+++ app/destination_routes.py
@@ -80,7 +80,9 @@ def create_destination():
         return (jsonify({"message": "An internal server error occurred"}), 500)
 
 
-@destination_bp.route("/destinations/<stream_key>", methods=["GET", "DELETE"])
+@destination_bp.route(
+    "/destinations/<stream_key>", methods=["GET", "DELETE", "PATCH"]
+)
 def get_destinations(stream_key):
     try:
         stream = Stream.query.filter_by(stream_key=stream_key).first()
@@ -102,6 +104,8 @@ def get_destinations(stream_key):
                 return get_destination(stream_key=stream_key, id=id)
             if request.method == "DELETE":
                 return delete_destination(stream_key=stream_key, id=id)
+            if request.method == "PATCH":
+                return update_destination(stream_key=stream_key, id=id)
 
         destinations = stream.destinations
 
@@ -207,6 +211,73 @@ def delete_destination(stream_key, id):
                 {
                     "message": "Destination deleted successfully!",
                     "destination": {"id": id},
+                }
+            ),
+            200,
+        )
+
+    except Exception:
+        return (jsonify({"message": "An internal server error occurred"}), 500)
+
+
+# @destination_bp.route("/destinations/<stream_key>/<id>", methods=["PATCH"])
+def update_destination(stream_key, id):
+    try:
+        stream = Stream.query.filter_by(stream_key=stream_key).first()
+
+        if not stream:
+            return (
+                jsonify(
+                    {
+                        "message": "The 'stream_key' does not exist",
+                        "destination": {},
+                    }
+                ),
+                400,
+            )
+
+        data = request.get_json()
+
+        if not data:
+            return (
+                jsonify({"message": "Invalid JSON body provided"}),
+                400,
+            )
+
+        live = data.get("live")
+
+        if not live:
+            return (
+                jsonify({"message": "The 'live' field is required"}),
+                400,
+            )
+
+        if live != "ON" and live != "OFF":
+            return (
+                jsonify({"message": "The 'live' field must be 'ON' or 'OFF'"}),
+                400,
+            )
+
+        destination = (
+            Destination.query.join(Stream)
+            .filter(Stream.stream_key == stream_key, Destination.id == id)
+            .first()
+        )
+
+        if not destination:
+            return (
+                jsonify({"message": "The 'id' does not exist"}),
+                400,
+            )
+
+        destination.live = live
+        db.session.commit()
+
+        return (
+            jsonify(
+                {
+                    "message": "Destination updated successfully!",
+                    "destination": ast.literal_eval(str(destination)),
                 }
             ),
             200,
