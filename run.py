69f903209574bebfdbb00d642adc07aa8bd05122
Welton Leite <av4t@hotmail.com>
2025-01-07 14:59:27 -0300

    chore: start from scratch to use FastAPI

--- run.py
+++
@@ -1,9 +0,0 @@
-from flask.cli import load_dotenv
-
-from app import create_app
-
-load_dotenv()
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
