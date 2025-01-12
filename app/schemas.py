e66bb9d830169a8861942d80c93e2fce343fc5e3
Welton Leite <av4t@hotmail.com>
2025-01-12 07:36:01 -0300

    refactor: improve schemas

--- app/schemas.py
+++ app/schemas.py
@@ -4,13 +4,13 @@ from pydantic import BaseModel
 
 
 class DestinationBase(BaseModel):
+    pass
+
+
+class DestinationCreate(BaseModel):
     stream_key: str
     dest_name: str
     dest_url: str
-
-
-class DestinationCreate(DestinationBase):
-    pass
 
 
 class DestinationUpdate(BaseModel):
@@ -20,7 +20,10 @@ class DestinationUpdate(BaseModel):
 class Destination(DestinationBase):
     id: int
     stream_id: int
+    stream_key: str
     pid: int
+    dest_name: str
+    dest_url: str
     live: bool
 
     class Config:
